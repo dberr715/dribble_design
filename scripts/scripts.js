f5a9a2d48455c4dc0d76d025e113779114d08fc4
David Berry <dberry3755@gmail.com>
2023-09-09 15:12:05 -0400

    Matched the main with JS updates.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,24 @@
-document.addEventListener.apply("click", function{
-    const
-})
+const modal = document.getElementById("myModal");
+const submitButton = document.querySelector(
+  "#contactForm input[type='submit']"
+);
+const closeButton = document.querySelector(".close");
+
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+
+  const message = `Thank you ${firstName} ${lastName} for submitting. Our team will be in touch soon!`;
+
+  const modalMessage = document.getElementById("modalMessage");
+  modalMessage.textContent = message;
+
+  modal.style.display = "block";
+});
+
+closeButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  modal.style.display = "none";
+});
